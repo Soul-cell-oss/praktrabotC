cad44b01804a4dab507351456497376a0ceb557f
Soul-cell-oss <larikamigos2@gmail.com>
2025-04-24 10:14:15 +0300

    “sdasd”

---
+++ Prakt 7.2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    delegate double QuadraticFunction(double x);
+
+    static QuadraticFunction GetQuadraticFunction(double a, double b, double c)
+    {
+        return x => a * x * x + b * x + c;
+    }
+
+    static void Main()
+    {
+        // Пример использования
+        var quadFunc = GetQuadraticFunction(1, 2, 1); // x² + 2x + 1
+
+        Console.WriteLine(quadFunc(0)); // 1
+        Console.WriteLine(quadFunc(1)); // 4
+        Console.WriteLine(quadFunc(2)); // 9
+    }
+}
---
+++ Prakt7.1/Prakt7.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    delegate string GetDayDelegate();
+
+    static void Main()
+    {
+        int currentDay = 0;
+        string[] days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+        GetDayDelegate getDay = () => {
+            string day = days[currentDay % 7];
+            currentDay++;
+            return day;
+        };
+
+        // Тестирование
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(getDay());
+        }
+    }
+}
